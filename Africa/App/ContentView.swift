f2b5abbbdcc5d509ed4d7e356da48700e0f1a531
Abiodun Osagie <abiodunosagie8@gmail.com>
2025-03-12 15:49:00 +0100

    Created two buttons to switch my layout

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,24 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
+    
+    
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(
-                        destination: AnimalDetailView(animal: animal)) {
-                            AnimalListItemView(animal: animal)
-                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                        }//: LINK                    
-                }//: LOOP
-            }//: LIST
+            Group {
+                
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(
+                                destination: AnimalDetailView(animal: animal)) {
+                                    AnimalListItemView(animal: animal)
+                                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                }//: LINK
+                        }//: LOOP
+                    }//: LIST
+                } else {
+                    Text("Grid view is active")
+                }//: CONDITION
+            }//: GROUP
             .listStyle(PlainListStyle())
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button {
+                            print("List view  is activated!")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        } label: {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(
+                                    isGridViewActive ? .primary : .accentColor
+                                )
+                        }
+                        
+                        // GRID
+                        Button{
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }label: {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(
+                                    isGridViewActive ? .accentColor : .primary
+                                )
+                        }
+                    }//: HSTACK
+                }
+            }
         } //: NAVIGATION
        
     }
