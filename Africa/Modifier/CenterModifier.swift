42948d6b6f5faea4cddfce15aa563f0efc373156
Abiodun Osagie <abiodunosagie8@gmail.com>
2025-03-13 12:10:08 +0100

    Macbook app update.

---
+++ Africa/Modifier/CenterModifier.swift
@@ -0,0 +1,8 @@
+//
+//  CenterModifier.swift
+//  Africa
+//
+//  Created by Abiodun Osagie on 13/03/2025.
+//
+
+import Foundation
---
+++ Africa/View/CopyrightsView.swift
@@ -0,0 +1,18 @@
+//
+//  CopyrightsView.swift
+//  Africa
+//
+//  Created by Abiodun Osagie on 13/03/2025.
+//
+
+import SwiftUI
+
+struct CopyrightsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CopyrightsView()
+}
