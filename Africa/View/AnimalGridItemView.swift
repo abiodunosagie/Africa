d54f5da8bb6e838a3bd37b1ca8519ec42be10a3c
Abiodun Osagie <abiodunosagie8@gmail.com>
2025-03-13 11:28:20 +0100

    Created a dynamic toolbaricon that organizes the list of animals.

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Abiodun Osagie on 13/03/2025.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalGridItemView()
+}
